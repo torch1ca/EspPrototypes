677a74b6791b78d66908427088ad818890d44812
arcarijo <joshua.arcari@gmail.com>
2024-07-19 20:16:40 -0400

    Add some ugly ass smoothing code to accelerometer
    -recursive low-pass filter for smoothing
    -deadzone
        --absolute deadzone at 0.3 * var
        --lerps to zero between 0.3-1 * var

--- AccelerometerEspNow/AccelerometerEspNow/AccelerometerEspNow.ino
+++ AccelerometerEspNow/AccelerometerEspNow/AccelerometerEspNow.ino
@@ -5,8 +5,6 @@
 
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-
 struct Accel {
   int16_t x = 0;
   int16_t y = 0;
@@ -16,6 +14,12 @@ struct Accel {
     memcpy(pBuf + 2, &y, 2);
   }
 };
+
+float deadZone = 3000;
+float filterWeight = 0.98;
+Accel estAccel;
+Accel prevEstAccel;
+long long accelIterations = 0;
 
 static void send_cb(const uint8_t *mac_address, esp_now_send_status_t status){
   send_cb_received = true;
@@ -48,11 +52,22 @@ void loop() {
   Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
   Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
   
-  Accel accel;
+  Accel currAccel;
   // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accel.x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accel.y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  currAccel.x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  if (abs(currAccel.x) < deadZone * 0.3f) currAccel.x = 0;
+  else if (abs(currAccel.x) < deadZone) currAccel.x = (currAccel.x / abs(currAccel.x)) * ((deadZone * 0.3f) + (abs(currAccel.x) - (deadZone * 0.3f)) * ((abs(currAccel.x) - deadZone * 0.3f) / (deadZone * 0.7f)));
+  currAccel.y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  if (abs(currAccel.y) < deadZone * 0.3f) currAccel.y = 0;
+  else if (abs(currAccel.y) < deadZone) currAccel.y = (currAccel.y / abs(currAccel.y)) * ((deadZone * 0.3f) + (abs(currAccel.y) - (deadZone * 0.3f)) * ((abs(currAccel.y) - deadZone * 0.3f) / (deadZone * 0.7f)));
   
+  int16_t x = filterWeight * filterWeight * prevEstAccel.x + filterWeight * (1.0f - filterWeight) * estAccel.x + (1.0f - filterWeight) * currAccel.x;
+  int16_t y = filterWeight * filterWeight * prevEstAccel.y + filterWeight * (1.0f - filterWeight) * estAccel.y + (1.0f - filterWeight) * currAccel.y;
+  prevEstAccel.x = estAccel.x; 
+  prevEstAccel.y = estAccel.y;
+  estAccel.x = x; 
+  estAccel.y = y;
+
   // // print out data
   // Serial.print("aX = "); Serial.print(convert_int16_to_str(accel.x));
   // Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accel.y));
@@ -61,13 +76,13 @@ void loop() {
   if (send_cb_received){
     send_cb_received = false;
     uint8_t *pBuf = (uint8_t *) malloc(4);
-    memcpy(pBuf, &accel.x, 2);
-    memcpy(pBuf + 2, &accel.y, 2);
+    memcpy(pBuf, &estAccel.x, 2);
+    memcpy(pBuf + 2, &estAccel.y, 2);
     esp_now_send(APMac, pBuf, sizeof(int));
     free(pBuf);
 
-    Serial.print("aX = "); Serial.print(convert_int16_to_str(accel.x));
-    Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accel.y));
+    Serial.print("aX = "); Serial.print(convert_int16_to_str(estAccel.x));
+    Serial.print(" | aY = "); Serial.print(convert_int16_to_str(estAccel.y));
     Serial.println();
   }
 }
